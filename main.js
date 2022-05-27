97bc0a6060d0121d146fe1594fc42cc93695d314
TheDeola <aadetunji012@gmail.com>
2022-05-27 18:38:20 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// This is my Javascript file
+
+const ratings = document.querySelector(".button"); 
+// console.log(ratings);
+
+const ratingsText = document.getElementById("ratingsText");
+// console.log(ratingsText);
+
+const button = document.querySelector("button");
+console.log(button);
+
+const btns = document.querySelectorAll(".btn");
+console.log(btns);
+
+let reviewText = 0;
+let currentBtn
+
+btns.forEach(function(btn){
+    btn.addEventListener("click", function(){
+        btn.classList.add("review");
+        reviewText = btn.innerHTML;
+        currentBtn = btn;
+    })
+})
+
+button.addEventListener("click", function(){
+    ratingsText.innerHTML = reviewText;
+    currentBtn.classList.toggle("review");
+})
